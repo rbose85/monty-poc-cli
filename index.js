7db3969ca4d8ef7671d47f0121ebd9a2fc57b0eb
Rajiv Bose <rbose85@users.noreply.github.com>
2016-05-15 12:49:59 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const launcher = require('./lib/duplicate-products')
+
+console.log('before ... ') // eslint-disable-line no-console
+
+launcher()
+
+console.log('... after') // eslint-disable-line no-console
